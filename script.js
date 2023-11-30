ec7732dfa5f4e2fd08fdaa2678e3da9ba8007fc0
Sebastian <sebastianristy@gmail.com>
2023-11-29 19:07:31 -0600

    test

---
+++ script.js
@@ -0,0 +1,111 @@
+// https://github.com/gabby-skelton/login-page
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAH9I4sTWl6eTf_DcHQwJG98E6N1VVTAt8",
+    authDomain: "seniordesign-5c156.firebaseapp.com",
+    databaseURL: "https://seniordesign-5c156-default-rtdb.firebaseio.com",
+    projectId: "seniordesign-5c156",
+    storageBucket: "seniordesign-5c156.appspot.com",
+    messagingSenderId: "47607103394",
+    appId: "1:47607103394:web:573778608fb2aaf4b03cfb",
+    measurementId: "G-1XQNS365JS"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const auth = getAuth(app);
+  
+  const submitButton = document.getElementById("submit");
+  const signupButton = document.getElementById("sign-up");
+  const emailInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
+  const main = document.getElementById("main");
+  const createacct = document.getElementById("create-acct")
+  
+  const signupEmailIn = document.getElementById("email-signup");
+  const confirmSignupEmailIn = document.getElementById("confirm-email-signup");
+  const signupPasswordIn = document.getElementById("password-signup");
+  const confirmSignUpPasswordIn = document.getElementById("confirm-password-signup");
+  const createacctbtn = document.getElementById("create-acct-btn");
+  
+  const returnBtn = document.getElementById("return-btn");
+  
+  var email, password, signupEmail, signupPassword, confirmSignupEmail, confirmSignUpPassword;
+  
+  console.log("Script loaded successfully");
+
+  createacctbtn.addEventListener("click", function() {
+    console.log("Create account button clicked");
+    var isVerified = true;
+  
+    signupEmail = signupEmailIn.value;
+    confirmSignupEmail = confirmSignupEmailIn.value;
+    if(signupEmail != confirmSignupEmail) {
+        window.alert("Email fields do not match. Try again.")
+        isVerified = false;
+    }
+  
+    signupPassword = signupPasswordIn.value;
+    confirmSignUpPassword = confirmSignUpPasswordIn.value;
+    if(signupPassword != confirmSignUpPassword) {
+        window.alert("Password fields do not match. Try again.")
+        isVerified = false;
+    }
+    
+    if(signupEmail == null || confirmSignupEmail == null || signupPassword == null || confirmSignUpPassword == null) {
+      window.alert("Please fill out all required fields.");
+      isVerified = false;
+    }
+    
+    if(isVerified) {
+      createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
+        .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+        window.alert("Success! Account created.");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        window.alert("Error occurred. Try again.");
+      });
+    }
+  });
+  
+  submitButton.addEventListener("click", function() {
+    email = emailInput.value;
+    console.log(email);
+    password = passwordInput.value;
+    console.log(password);
+  
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log("Success! Welcome back!");
+        window.alert("Success! Welcome back!");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log("Error occurred. Try again.");
+        window.alert("Error occurred. Try again.");
+      });
+  });
+  
+  signupButton.addEventListener("click", function() {
+      main.style.display = "none";
+      createacct.style.display = "block";
+  });
+  
+  returnBtn.addEventListener("click", function() {
+      main.style.display = "block";
+      createacct.style.display = "none";
+  });
